ad13931df028b513aa89a01ef54a7b17e9aff0c4
glueckself <e1025857@student.tuwien.ac.at>
2012-10-23 09:48:33 +0200

    Beispiel 2 fertig

--- Aufgabe1.hs
+++ Aufgabe1.hs
@@ -1,7 +1,22 @@
--- katNumber n = (1/(fromIntegral (n+1)))*((product [1..(2*n)])/(fromIntegral ((product [1..n])*(product [1..n]))))
+-- Author: Srdjan Markovic (1025857)
+
+-- Bsp. 1
 katNumber :: Integer -> Integer
 
 katNumber 0 = error "Argument ungueltig"
 katNumber k = let n=k-1 in
               div (product [1..(2*n)]) ((n+1)*(product [1..n])*(product [1..n]))
+
+-- Bsp. 2
+-- Hilfsfunktion zum Berechen
+
+sumPowers :: Integer -> Integer -> Integer
+
+sumPowers n k = 
+    if k < 0
+        then (-1)
+        else if n < 0
+            then (0)
+            else sum (map (^k) [1..n])
+
 
