cf780f7bd26c992794fd8f8e04d9abc6fb01aa50
glueckself <e1025857@student.tuwien.ac.at>
2012-10-27 17:25:28 +0200

    Bsp 2 fertig

--- Aufgabe2.hs
+++ Aufgabe2.hs
@@ -1,17 +1,41 @@
+import Data.List
+
 -- Bsp. 1
+-- "kgv" koennte "agv" benutzen, um dann nur das erste Element auszuwaehlen.
+-- Da "agv" jedoch mehr Rechenaufwand hat und der bereich 0-unendlich waere
+-- verwende ich hier den Zusammenhang zw. ggT und kgV
+-- Alternativ waere eine Primfaktorzerlegung auch moeglich gewesen.
+
 -- Hilfsfunktion ggt
 ggt :: Integer -> Integer -> Integer
 ggt m n 
       | n == 0 = m
       | otherwise = ggt n (mod m n)
 
+-- kgv
 kgv :: Integer -> Integer -> Integer
-
 kgv m n = let a = abs(m) in
           let b = abs(n) in
                 if (a == 0) then 0
                 else if (b == 0) then 0
                      else div (a*b) (ggt a b)
+
+-- Bsp. 2
+vf :: Integer -> (Integer,Integer) -> [Integer]
+vf a (p,q) = let p1 = if (p > 0) then div p a 
+                                 else 1
+                                 in
+             let q1 = if (q > 0) then div q a 
+                                 else 1
+                                 in
+                                     map (*a) [p1..q1]
+--
+agv :: Integer -> Integer -> (Integer,Integer) -> [Integer]
+agv m n (p,q) = let a = abs(m) in
+                let b = abs(n) in
+                      if (a == 0) then []
+                      else if (b == 0) then []
+                           else nub (intersect (vf a (p,q)) (vf b (p,q)))
 
 -- Bsp. 3/4/5
 
