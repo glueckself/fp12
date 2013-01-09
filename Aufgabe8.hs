3b17d57eaa39bf51b5ad469ec8f024f852928e94
glueckself <e1025857@student.tuwien.ac.at>
2013-01-09 14:33:40 +0100

    Aufgabe 8/9

--- Aufgabe8.hs
+++ Aufgabe8.hs
@@ -24,6 +24,8 @@ addRev' round maxRounds value | round > maxRounds = Failure
 addRev :: InitialValue -> MaxRounds -> Solution
 addRev inVal maxRounds = addRev' 0 (abs maxRounds) (abs inVal)
 
+
+
 type Country = String
 type Countries = [Country]
 type TravelTime = Integer -- Travel time in minutes
@@ -36,6 +38,15 @@ data Itinerary = NoRoute | Route (Connections,TravelTime) deriving (Eq,Ord,Show)
 
 type SimplConnections = [(Country,Country,TravelTime)]
 
+simDb = [(Road "at" "hu" 10),
+         (Road "hu" "sr" 20),
+         (Road "at" "de" 10),
+         (Road "de" "fr" 30),
+         (Road "it" "at" 10),
+         (Air "at" "at" 2),
+         (Sea "it" "fr" 30),
+         (Air "fr" "sr" 5)]
+         
 fst' (x,_,_) = x
 snd' (_,x,_) = x
 trd' (_,_,x) = x
@@ -46,27 +57,6 @@ simplCon (Sea c1 c2 t) =  (c1,c2,t)
 simplCon (Rail c1 c2 t) = (c1,c2,t)
 simplCon (Road c1 c2 t) = (c1,c2,t)
 
-getStart :: Connection -> Country
-getStart c = fst' (simplCon c)
-
-getDest :: Connection -> Country
-getDest c = snd' (simplCon c)
-
-getTime :: Connection -> TravelTime
-getTime c = trd' (simplCon c)
-
-fixEntry :: Connection -> Connection
-fixEntry (Air c1 c2 t) = Air c1 c2 (abs t)
-fixEntry (Sea c1 c2 t) =  Sea c1 c2 (abs t)
-fixEntry (Rail c1 c2 t) = Rail c1 c2 (abs t)
-fixEntry (Road c1 c2 t) = Road c1 c2 (abs t)
-
-fixDb :: Connections -> Connections
-fixDb db = map fixEntry db
-
-neighbours :: Connections -> Country -> Connections
-neighbours db country = filter isInConnection db
-                        where isInConnection x = (getStart x) == country || (getDest x) == country
 
 expandList :: SimplConnections -> [Country] -> [Country]
 expandList db crt = union [ snd' x | x <- db, elem (fst' x) crt ]
@@ -81,33 +71,6 @@ isRoute' db crtList dst = if elem dst crtList
                                       if lenN > lenC
                                          then isRoute' db newList dst
                                          else False
-
-yieldFastestRoute :: Connections -> Country -> Country -> Connections
-yieldFastestRoute db start dest = []
-
-yieldFeelGoodRoute :: Connections -> Country -> Country -> TravelTime -> Connections
-yieldFeelGoodRoute db start dest time = []
-
-
--- Da yieldRoute auch die kurzeste Strecke anzeigen kann, ist hier keine andere Implementierung notwendig
-yieldRoute :: Connections -> Country -> Country -> Connections
-yieldRoute db start dest = yieldFastestRoute db start dest
-                               
-
+                                         
 isRoute :: Connections -> Country -> Country -> Bool
-isRoute db start dest = if (yieldRoute db start dest == [])
-                           then False
-                           else True
-
-isFeelGoodRoute :: Connections -> Country -> Country -> TravelTime -> Bool
-isFeelGoodRoute db start dest time = if (yieldFeelGoodRoute db start dest time == [])
-                                        then False
-                                        else True
-
-simDb = [(Road "at" "hu" 10),
-         (Road "hu" "sr" 20),
-         (Road "at" "de" 10),
-         (Road "de" "fr" 30),
-         (Road "it" "at" 10),
-         (Sea "it" "fr" 30),
-         (Air "fr" "sr" 5)]
+isRoute db start dest = isRoute' (map simplCon db) [start] dest
--- Aufgabe9.hs
+++ Aufgabe9.hs
@@ -1,5 +1,67 @@
 import Data.List
 
+-- Beispiel 1
+type Country = String
+type Countries = [Country]
+type TravelTime = Integer -- Travel time in minutes
+data Connection = Air Country Country TravelTime
+                  | Sea Country Country TravelTime
+                  | Rail Country Country TravelTime
+                  | Road Country Country TravelTime deriving (Eq,Ord,Show)
+type Connections = [Connection]
+data Itinerary = NoRoute | Route (Connections,TravelTime) deriving (Eq,Ord,Show)
+
+type SimplConnections = [(Country,Country,TravelTime)]
+
+fst' (x,_,_) = x
+snd' (_,x,_) = x
+trd' (_,_,x) = x
+
+-- Hilfsfunktion, um Daten vom Konstruktor unabhaengig zu machen
+simplCon :: Connection -> (Country,Country,TravelTime)
+simplCon (Air c1 c2 t) = (c1,c2,t)
+simplCon (Sea c1 c2 t) =  (c1,c2,t)
+simplCon (Rail c1 c2 t) = (c1,c2,t)
+simplCon (Road c1 c2 t) = (c1,c2,t)
+
+-- Hilfsfunktion, die beide Leander zurueckliefert
+getCountries :: Connection -> Countries
+getCountries e = [fst' elem,snd' elem]
+                 where elem = simplCon e
+
+-- Erzeugt eine Liste aus der Datenbank
+flatDb :: Connections -> Countries
+flatDb db = concatMap (\e -> getCountries e) db
+
+-- Erweitert eine Liste um alle Nachbarn ihrer Elemente
+expandList :: Connections -> Countries -> Countries
+expandList fullDb crt = union [ snd' x | x <- db, elem (fst' x) crt ] (union [ fst' x | x <- db, elem (snd' x) crt ] crt)
+                    where db = map simplCon fullDb
+
+-- Sucht alle Nachbarn eines Landes
+getAllNeighbours :: Connections -> Countries -> Countries
+getAllNeighbours db crtList = let newList = expandList db crtList
+                                  lenN = length newList
+                                  lenC = length crtList in
+                                  if lenN > lenC
+                                     then getAllNeighbours db newList
+                                     else crtList
+
+yieldUnreachable :: Connections -> Country -> Countries
+yieldUnreachable db cntry = nub (filter (\e -> notElem e (getAllNeighbours db [cntry])) (flatDb db))
+
+-- Hilfsfunktion, die nur Rail oder Road verbindungen zurueckgibt
+filterDb :: Connections -> Connections
+filterDb db = filter rules db
+              where rules (Air _ _ _) = False
+                    rules (Sea _ _ _) = False
+                    rules (Rail _ _ _) = True
+                    rules (Road _ _ _) = True
+
+yieldGroundReachable :: Connections -> Country -> Countries
+yieldGroundReachable db cntry = getAllNeighbours (filterDb db) [cntry]
+
+-- Beispiel 2
 -- Hilfsfunktion, die die Position "unbedingt" zurueckliefert
 getPos :: [Integer] -> Integer -> Integer
 getPos lst x = let pos = elemIndex x lst in
@@ -27,3 +89,13 @@ yieldLuckyNumbers min max = filter (\e -> (min <= e) ) (takeWhile (<=max) luckyN
 
 isTwinLuckyNumber :: Integer -> Bool
 isTwinLuckyNumber n = (isLuckyNumber n)&&( (isLuckyNumber (n+2))||(isLuckyNumber (n-2)) )
+
+
+simDb = [(Road "at" "hu" 10),
+         (Road "hu" "sr" 20),
+         (Road "at" "de" 10),
+        -- (Road "de" "fr" 30),
+         (Road "it" "at" 10),
+         (Sea "it" "fr" 30),
+         (Air "fr" "sr" 5),
+         (Air "usa" "ca" 5)]
